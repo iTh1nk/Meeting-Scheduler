e6f3c13e09520967db6a1f7184e20777717e6657
iTh1nk <fnchao@hotmail.com>
2020-06-28 23:55:51 -0700

    setting up login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from "react";
 import { AssignContext } from "../AssignContext";
-import { Modal, Button } from "antd";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Modal, Button, Form, Input } from "antd";
 import { HomeOutlined } from "@ant-design/icons";
 
 export default function Login() {
@@ -52,7 +54,91 @@ export default function Login() {
           </Button>,
         ]}
       >
-        Login Form Display
+        <Formik
+          initialValues={{ guid: "", gpin: "" }}
+          validationSchema={Yup.object({
+            irvineDataId: Yup.number()
+              .typeError("This filed requires a number!")
+              .positive("ID has to be a positive number!")
+              .integer("ID has to be an integer!"),
+            irvineDataDate: Yup.string()
+              .required("Date is required!")
+              .matches(
+                /[0-1][0-9]\-[0-3][0-9]$/g,
+                "Date format is invalid! (i.e. 01-23)"
+              ),
+            irvineDataConfirmed: Yup.number()
+              .typeError("This filed requires a number!")
+              .required("The number of confirmed cases is required!")
+              .positive("Confirmed number has to be positive!")
+              .integer("Confirmed number has to be an integer!"),
+          })}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            setSubmitting(true);
+            this.handleOnClickSubmit();
+            setTimeout(() => {
+              resetForm();
+              setSubmitting(false);
+            }, 2000);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleSubmit,
+            handleBlur,
+            isSubmitting,
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  { required: true, message: "Please input your username!" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+              <br />
+              <Button
+                variant="outline-success"
+                size="sm"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Submit
+              </Button>
+              <Button
+                variant="outline-secondary"
+                size="sm"
+                style={{ marginLeft: "10px" }}
+                onClick={(event) => {
+                  this.handleOnClickReset(event);
+                }}
+              >
+                Reset
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </div>
   );
