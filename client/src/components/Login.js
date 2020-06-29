59f37d68542a039af128c13d6e8f199e3a897677
iTh1nk <fnchao@hotmail.com>
2020-06-29 09:16:45 -0700

    Login form integrated with antd

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -30,68 +30,59 @@ export default function Login() {
 
   return (
     <div>
-      <Modal
-        title="Login"
-        visible={visible}
-        onOk={(e) => handleOk(e)}
-        confirmLoading={confirmLoading}
-        onCancel={(e) => handleCancel(e)}
-        footer={[
-          <Button
-            key="back"
-            icon={<HomeOutlined />}
-            onClick={(e) => handleCancel(e)}
-          >
-            Home
-          </Button>,
-          <Button
-            key="submit"
-            type="primary"
-            loading={confirmLoading}
-            onClick={(e) => handleOk(e)}
-          >
-            Login
-          </Button>,
-        ]}
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={Yup.object({
+          username: Yup.string(),
+          password: Yup.string()
+            .matches(
+              /[0-1][0-9]\-[0-3][0-9]$/g,
+              "Date format is invalid! (i.e. 01-23)"
+            ),
+        })}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          alert("Submitted!");
+          setTimeout(() => {
+            resetForm();
+            setSubmitting(false);
+          }, 2000);
+        }}
       >
-        <Formik
-          initialValues={{ guid: "", gpin: "" }}
-          validationSchema={Yup.object({
-            irvineDataId: Yup.number()
-              .typeError("This filed requires a number!")
-              .positive("ID has to be a positive number!")
-              .integer("ID has to be an integer!"),
-            irvineDataDate: Yup.string()
-              .required("Date is required!")
-              .matches(
-                /[0-1][0-9]\-[0-3][0-9]$/g,
-                "Date format is invalid! (i.e. 01-23)"
-              ),
-            irvineDataConfirmed: Yup.number()
-              .typeError("This filed requires a number!")
-              .required("The number of confirmed cases is required!")
-              .positive("Confirmed number has to be positive!")
-              .integer("Confirmed number has to be an integer!"),
-          })}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            setSubmitting(true);
-            this.handleOnClickSubmit();
-            setTimeout(() => {
-              resetForm();
-              setSubmitting(false);
-            }, 2000);
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleSubmit,
-            handleBlur,
-            isSubmitting,
-          }) => (
-            <Form onSubmit={handleSubmit}>
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleSubmit,
+          handleBlur,
+          isSubmitting,
+        }) => (
+          <Modal
+            title="Login"
+            visible={visible}
+            onOk={(e) => handleOk(e)}
+            confirmLoading={confirmLoading}
+            onCancel={(e) => handleCancel(e)}
+            footer={[
+              <Button
+                key="back"
+                icon={<HomeOutlined />}
+                onClick={(e) => handleCancel(e)}
+              >
+                Home
+              </Button>,
+              <Button
+                key="submit"
+                type="primary"
+                loading={confirmLoading}
+                onClick={(e) => handleSubmit(e)}
+              >
+                Login
+              </Button>,
+            ]}
+          >
+            <Form>
               <Form.Item
                 label="Username"
                 name="username"
@@ -101,7 +92,6 @@ export default function Login() {
               >
                 <Input />
               </Form.Item>
-
               <Form.Item
                 label="Password"
                 name="password"
@@ -111,35 +101,10 @@ export default function Login() {
               >
                 <Input.Password />
               </Form.Item>
-
-              <Form.Item>
-                <Button type="primary" htmlType="submit">
-                  Submit
-                </Button>
-              </Form.Item>
-              <br />
-              <Button
-                variant="outline-success"
-                size="sm"
-                type="submit"
-                disabled={isSubmitting}
-              >
-                Submit
-              </Button>
-              <Button
-                variant="outline-secondary"
-                size="sm"
-                style={{ marginLeft: "10px" }}
-                onClick={(event) => {
-                  this.handleOnClickReset(event);
-                }}
-              >
-                Reset
-              </Button>
             </Form>
-          )}
-        </Formik>
-      </Modal>
+          </Modal>
+        )}
+      </Formik>
     </div>
   );
 }
